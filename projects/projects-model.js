e4eac433e6cd5d66cbbcf093f91d639f9a6102ee
Eric Ferguson <turtle0888@gmail.com>
2020-02-21 12:46:07 -0600

    Add projects-model

---
+++ projects/projects-model.js
@@ -0,0 +1,55 @@
+const db = require('../data/db-config.js');
+
+module.exports = {
+  getProjects,
+  getById,
+  getResources,
+  getTasks,
+  addProjects,
+  addResources,
+  addTasks
+};
+
+function getProjects() {
+  return db('projects');
+}
+
+function getProjects(id) {
+  return db('projects')
+    .where({ id })
+    .first();
+}
+
+function getResources(id) {
+  return db('project_details as pd')
+    .select('pd.project_id', 'r.id', 'r.name', 'r.description')
+    .join('resources as r', 'pd.resource_id, r.id')
+    .where('pd.project_id', id);
+}
+
+function getTasks(id) {
+  return db('tasks as t')
+    .select('t.id', 't.description', 't.notes', 't.completed')
+    .join('projects as p', 'p.id', 't.project_id')
+    .where('p.project_id', id);
+}
+
+function addProjects(project) {
+  return db('projects').insert(project, id);
+}
+
+function addResources(resource) {
+  return db('resources')
+    .insert(resource, id)
+    .then(ids => {
+      return getById(ids[0]);
+    });
+}
+
+function addTasks(task) {
+  return db('tasks')
+    .insert(task, id)
+    .then(ids => {
+      return getById(ids[0]);
+    });
+}
